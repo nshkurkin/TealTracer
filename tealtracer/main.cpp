84ce9a62463f6adde36da415af6df2810e37c25b
Nikolai Shkurkin <nshkurkin@gmail.com>
2016-03-29 13:43:39 -0700

    Initial Commit

---
+++ tealtracer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tealtracer
+//
+//  Created by Nikolai Shkurkin on 3/29/16.
+//  Copyright © 2016 Teal Sunset Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
