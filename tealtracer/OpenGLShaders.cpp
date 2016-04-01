bc25750b6be4e39543f8fdbfa3f4a533aa4b74d4
Nikolai Shkurkin <nshkurkin@gmail.com>
2016-03-31 22:41:09 -0700

    Added a preliminary port of the shader code from original project

---
+++ tealtracer/OpenGLShaders.cpp
@@ -0,0 +1,420 @@
+//
+//  OpenGLShaders.cpp
+//  tealtracer
+//
+//  Created by Nikolai Shkurkin on 3/31/16.
+//  Copyright © 2016 Teal Sunset Studios. All rights reserved.
+//
+
+#include "OpenGLShaders.hpp"
+
+///
+bool
+OpenGLShaderVariable::isArray() const {
+    return arraySize > 1;
+}
+
+///
+bool
+OpenGLShaderVariable::valid() const {
+    return location >= 0;
+}
+
+///
+OpenGLShaderVariable::OpenGLShaderVariable() {
+    init("", Attribute, GLint(-1), GLint(0), GLint(0), GLenum(GL_NONE));
+}
+
+///
+OpenGLShaderVariable::OpenGLShaderVariable(const std::string & name, Type variableType) {
+    init(name, variableType, GLint(-1), GLint(0), GLint(0), GLenum(GL_NONE));
+}
+
+///
+OpenGLShaderVariable::OpenGLShaderVariable(const std::string & name, Type variableType, GLint location, GLint arraySize, GLint numElements, GLenum valueType) {
+    init(name, variableType, location, arraySize, numElements, valueType);
+}
+
+///
+void
+OpenGLShaderVariable::init(const std::string & name, Type variableType, GLint location, GLint arraySize, GLint numElements, GLenum valueType) {
+    this->name = name;
+    this->variableType = variableType;
+    this->location = location;
+    this->numElements = numElements;
+    this->valueType = valueType;
+    this->arraySize = arraySize;
+}
+
+///
+void
+OpenGLShaderVariable::glEnableAttribute() const {
+    assert(variableType == Attribute);
+    if (valid()) {
+        glEnableVertexAttribArray(GLuint(location));
+    }
+}
+
+///
+void
+OpenGLShaderVariable::glDisableAttribute() const {
+    assert(variableType == Attribute);
+    if (valid()) {
+        glDisableVertexAttribArray(GLuint(location));
+    }
+}
+///
+void
+OpenGLShaderVariable::glSetAttributeLayout() const {
+    assert(variableType == Attribute);
+    if (valid()) {
+        glVertexAttribPointer(GLuint(location), numElements, valueType, GLboolean(GL_FALSE), GLsizei(0), nullptr);
+    }
+}
+
+
+///
+bool
+OpenGLShader::isVertexShader() const {
+    return type == GLenum(GL_VERTEX_SHADER);
+}
+///
+bool
+OpenGLShader::isFragmentShader() const {
+    return type == GLenum(GL_FRAGMENT_SHADER);
+}
+///
+bool
+OpenGLShader::isGeometryShader() const {
+    return type == GLenum(GL_GEOMETRY_SHADER);
+}
+
+///
+bool
+OpenGLShader::compiledSuccessfully() const {
+    GLint status;
+    glGetShaderiv(handle(), GLenum(GL_COMPILE_STATUS), &status);
+    return status == GL_TRUE;
+}
+
+///
+OpenGLShader::OpenGLShader() {
+    init(GLenum(GL_VERTEX_SHADER), "", "");
+}
+
+///
+OpenGLShader::OpenGLShader(GLenum type, const std::string & source) {
+    init(type, source, "");
+}
+
+///
+OpenGLShader::OpenGLShader(GLenum type, const std::string & source, const std::string & filePath) {
+    init(type, source, filePath);
+}
+
+///
+void
+OpenGLShader::init(GLenum type, const std::string & source, const std::string & filePath) {
+    this->type = type;
+    this->source = source;
+    this->filePath = filePath;
+}
+
+///
+static inline std::string
+readFileContent(const std::string & filePath) {
+    std::ifstream source;
+    source.open(filePath.c_str(), std::ios_base::in);
+    if (!source) {
+        assert(false);
+    }
+    
+    std::string content;
+    std::string line;
+    while (!std::getline(source, line).eof()) {
+        /// Remove all comments
+        content.append(line);
+    }
+    
+    return content;
+}
+
+///
+std::shared_ptr<OpenGLShader>
+OpenGLShader::vertexShaderWithSource(const std::string & source) {
+    return std::shared_ptr<OpenGLShader>(new OpenGLShader(GLenum(GL_VERTEX_SHADER), source));
+}
+
+///
+std::shared_ptr<OpenGLShader>
+OpenGLShader::fragmentShaderWithSource(const std::string & source) {
+    return std::shared_ptr<OpenGLShader>(new OpenGLShader(GLenum(GL_FRAGMENT_SHADER), source));
+}
+
+///
+std::shared_ptr<OpenGLShader>
+OpenGLShader::geometryShaderWithSource(const std::string & source) {
+    return std::shared_ptr<OpenGLShader>(new OpenGLShader(GLenum(GL_GEOMETRY_SHADER), source));
+}
+
+///
+std::shared_ptr<OpenGLShader>
+OpenGLShader::vertexShaderWithFilePath(const std::string & filePath) {
+    return std::shared_ptr<OpenGLShader>(new OpenGLShader(GLenum(GL_VERTEX_SHADER), readFileContent(filePath), filePath));
+}
+
+///
+std::shared_ptr<OpenGLShader>
+OpenGLShader::fragmentShaderWithFilePath(const std::string & filePath) {
+    return std::shared_ptr<OpenGLShader>(new OpenGLShader(GLenum(GL_FRAGMENT_SHADER), readFileContent(filePath), filePath));
+}
+
+///
+std::shared_ptr<OpenGLShader>
+OpenGLShader::geometryShaderWithFilePath(const std::string & filePath) {
+    return std::shared_ptr<OpenGLShader>(new OpenGLShader(GLenum(GL_GEOMETRY_SHADER), readFileContent(filePath), filePath));
+}
+
+///
+std::shared_ptr<GLuint>
+OpenGLShader::allocateContent() {
+    return std::shared_ptr<GLuint>(new GLuint(glCreateShader(type)));
+}
+
+///
+void
+OpenGLShader::freeContent() {
+    glDeleteShader(handle());
+}
+
+///
+void
+OpenGLShader::compile() {
+    glAllocate();
+    
+    const char * lines[1] = { source.c_str() };
+    GLint length = (GLint) source.length();
+    
+    glShaderSource(handle(), GLsizei(1), lines, &length);
+    glCompileShader(handle());
+    
+    if (!compiledSuccessfully()) {
+        std::cerr << "{" << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")} Failed to compile shader with source: " << source << " \n\nStatus error: " << statusMessage() << std::endl;
+    }
+}
+
+///
+std::string
+OpenGLShader::statusMessage() const {
+    GLint logLength, outputLength;
+    glGetShaderiv(handle(), GL_INFO_LOG_LENGTH, &logLength);
+    char * buffer = new char[logLength + 1];
+    glGetShaderInfoLog(handle(), logLength, &outputLength, buffer);
+    std::string message(buffer);
+    delete[] buffer;
+    return message;
+}
+
+
+///
+GLboolean
+OpenGLProgram::linkSuccess() const {
+    GLint status;
+    glGetProgramiv(handle(), GLenum(GL_LINK_STATUS), &status);
+    return GLboolean(status);
+}
+
+///
+GLboolean
+OpenGLProgram::validationSuccess() const {
+    GLint status;
+    glGetProgramiv(handle(), GLenum(GL_VALIDATE_STATUS), &status);
+    return GLboolean(status);
+}
+
+///
+std::shared_ptr<GLuint>
+OpenGLProgram::allocateContent() {
+    return std::shared_ptr<GLuint>(new GLuint(glCreateProgram()));
+}
+
+///
+void
+OpenGLProgram::freeContent() {
+    glDeleteProgram(handle());
+}
+
+///
+void
+OpenGLProgram::build(bool crashOnFailure) {
+    glAllocate();
+
+    bool foundBadShader = false;
+    int whichShader = 0;
+    while (whichShader < shaders.size() && !foundBadShader) {
+        auto shader = shaders[whichShader];
+        shader->compile();
+        foundBadShader = !shader->compiledSuccessfully();
+        whichShader++;
+    }
+    
+    if (foundBadShader) {
+        std::cerr << "{" << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")} Shader " << (whichShader - 1) << " with source " << shaders[whichShader-1]->source << " failed to compile with error log: " << shaders[whichShader-1]->statusMessage() << std::endl;
+        assert(!crashOnFailure);
+    }
+    else {
+        for (auto itr = shaders.begin(); itr != shaders.end(); itr++) {
+            glAttachShader(handle(), (*itr)->handle());
+        }
+        
+        glLinkProgram(handle());
+        if (linkSuccess()) {
+            std::cerr << "{" << __FILE__ << ":" << __LINE__ << " (" << __FUNCTION__ << ")} Linking failed! OpenGL error: " << statusMessage() << std::endl;
+            assert(!crashOnFailure);
+        }
+        
+//            gatherAdditionalProgramInfo();
+        
+        for (auto itr = shaders.begin(); itr != shaders.end(); itr++) {
+            (*itr)->glFree();
+        }
+    }
+
+}
+
+///
+GLuint
+OpenGLProgram::setAsActiveProgram() {
+    GLint oldProgram;
+    glGetIntegerv(GLenum(GL_CURRENT_PROGRAM), &oldProgram);
+    glUseProgram(handle());
+    
+    return GLuint(oldProgram);
+}
+
+///
+void
+OpenGLProgram::restoreActiveProgram(GLuint oldProgram) const {
+    glUseProgram(oldProgram);
+}
+
+///
+GLint
+OpenGLProgram::attachedShaderCount() const {
+    GLint count;
+    glGetProgramiv(handle(), GLenum(GL_ATTACHED_SHADERS), &count);
+    return count;
+}
+
+///
+GLint
+OpenGLProgram::activeAttributeCount() const {
+    GLint count;
+    glGetProgramiv(handle(), GLenum(GL_ACTIVE_ATTRIBUTES), &count);
+    return count;
+}
+
+///
+GLint
+OpenGLProgram::activeUniformCount() const {
+    GLint count;
+    glGetProgramiv(handle(), GLenum(GL_ACTIVE_UNIFORMS), &count);
+    return count;
+}
+
+/// Returns the type information for the OpenGL type `type`. Type info with a
+/// name == "<unknown>" is not recognized by this function.
+static inline OpenGLTypeInfo getTypeInfoForGLType(GLenum type) {
+    static std::map<GLenum, OpenGLTypeInfo> info;
+
+    if (info.empty() == 0) {
+        #define setField(type, name, size, els, elType) \
+            info[GLenum(type)] = OpenGLTypeInfo(GLenum(type), name, GLsizei(size), GLuint(els), GLenum(elType))
+        
+        setField(GL_BYTE, "GLbyte", sizeof(GLbyte), 1, GL_BYTE);
+        setField(GL_UNSIGNED_BYTE, "GLubyte", sizeof(GLubyte), 1, GL_UNSIGNED_BYTE);
+        setField(GL_SHORT, "GLshort", sizeof(GLshort), 1, GL_SHORT);
+        setField(GL_UNSIGNED_SHORT, "GLushort", sizeof(GLushort), 1, GL_UNSIGNED_SHORT);
+        setField(GL_INT, "GLint", sizeof(GLint), 1, GL_INT);
+        setField(GL_UNSIGNED_INT, "GLuint", sizeof(GLuint), 1, GL_UNSIGNED_INT);
+        setField(GL_FLOAT, "GLfloat", sizeof(GLfloat), 1, GL_FLOAT);
+        setField(GL_BOOL, "GLboolean", sizeof(GLboolean), 1, GL_BOOL);
+        setField(GL_FLOAT_VEC2, "GLfloat[2]", 2 * sizeof(GLfloat), 2, GL_FLOAT);
+        setField(GL_FLOAT_VEC3, "GLfloat[3]", 3 * sizeof(GLfloat), 3, GL_FLOAT);
+        setField(GL_FLOAT_VEC4, "GLfloat[4]", 4 * sizeof(GLfloat), 4, GL_FLOAT);
+        setField(GL_FLOAT_MAT2, "GLfloat[2][2]", 2 * 2 * sizeof(GLfloat), 4, GL_FLOAT);
+        setField(GL_FLOAT_MAT3, "GLfloat[3][3]", 3 * 3 * sizeof(GLfloat), 9, GL_FLOAT);
+        setField(GL_FLOAT_MAT4, "GLfloat[4][4]", 4 * 4 * sizeof(GLfloat), 16, GL_FLOAT);
+        setField(GL_SAMPLER_2D, "<sampler 2D>", 0, 0, GL_SAMPLER_2D);
+        setField(GL_SAMPLER_3D, "<sampler 3D>", 0, 0, GL_SAMPLER_3D);
+        setField(GL_SAMPLER_CUBE, "<sampler cube>", 0, 0, GL_SAMPLER_CUBE);
+        setField(GL_SAMPLER_2D_SHADOW, "<sampler 2D shadow>", 0, 0, GL_SAMPLER_2D_SHADOW);
+    }
+    
+    if (info.count(type) != 0) {
+        return info[type];
+    }
+    else {
+        return OpenGLTypeInfo(type, "<unknown>", GLsizei(0), GLuint(0), type);
+    }
+}
+
+///
+void
+OpenGLProgram::gatherAdditionalProgramInfo() {
+    
+    activeAttributeMap.clear();
+    activeUniformMap.clear();
+    
+    GLint size;
+    GLenum type;
+    char * name;
+    GLint nameLength;
+    GLsizei actualNameLength;
+    
+    for (int attribIdx = 0; attribIdx < activeAttributeCount(); attribIdx++) {
+        
+        glGetProgramiv(handle(), GLenum(GL_ACTIVE_ATTRIBUTE_MAX_LENGTH), &nameLength);
+        name = new char[nameLength];
+        
+        glGetActiveAttrib(handle(), attribIdx, GLsizei(nameLength), &actualNameLength, &size, &type, name);
+        
+        auto typeInfo = getTypeInfoForGLType(type);
+        std::string attribName(name);
+        auto attribute = OpenGLShaderVariable(attribName, OpenGLShaderVariable::Attribute, glGetAttribLocation(handle(), attribName.c_str()), size, GLint(typeInfo.numElements), typeInfo.elementType);
+        
+        activeAttributeMap[attribName] = attribute;
+        
+        delete[] name;
+    }
+    
+    for (int uniformIdx = 0; uniformIdx < activeUniformCount(); uniformIdx++) {
+        const GLuint idx = uniformIdx;
+
+        glGetActiveUniformsiv(handle(), GLsizei(1), &idx, GLenum(GL_UNIFORM_NAME_LENGTH), &nameLength);
+        name = new char[nameLength];
+        
+        glGetActiveUniform(handle(), uniformIdx, GLsizei(nameLength), &actualNameLength, &size, &type, name);
+        
+        auto typeInfo = getTypeInfoForGLType(type);
+        std::string uniformName(name);
+        auto uniform = OpenGLShaderVariable(uniformName, OpenGLShaderVariable::Uniform, glGetUniformLocation(handle(), uniformName.c_str()), size, GLint(typeInfo.numElements), typeInfo.elementType);
+        
+        activeUniformMap[uniformName] = uniform;
+        
+        delete[] name;
+    }
+}
+
+///
+std::string
+OpenGLProgram::statusMessage() const {
+    GLint logLength, outputLength;
+    glGetProgramiv(handle(), GL_INFO_LOG_LENGTH, &logLength);
+    char * buffer = new char[logLength + 1];
+    glGetProgramInfoLog(handle(), logLength, &outputLength, buffer);
+    std::string message(buffer);
+    delete[] buffer;
+    return message;
+}
+
---
+++ tealtracer/OpenGLShaders.hpp
@@ -0,0 +1,262 @@
+//
+//  OpenGLShaders.hpp
+//  tealtracer
+//
+//  Created by Nikolai Shkurkin on 3/31/16.
+//  Copyright © 2016 Teal Sunset Studios. All rights reserved.
+//
+
+#ifndef OpenGLShaders_hpp
+#define OpenGLShaders_hpp
+
+#include "gl_include.h"
+#include "OpenGLObject.hpp"
+
+#include <map>
+#include <string>
+#include <vector>
+#include <memory>
+
+#include <iostream>
+#include <fstream>
+
+#include <cassert>
+
+/// Represents a connection to a shader program's variable. Variables include
+/// all of the types listed under `OpenGLShaderVariable.Type`. Typically you
+/// don't create one of these manually and instead get them from the
+//// `OpenGLProgram` instance that has already been compiled.
+///
+/// `Attribute`s are per-vertex data sent to shaders like "positions",
+/// "normals", etc.. One can set attribute layouts and to enable a particular
+/// attribute.
+///
+/// `Uniform`s constant data in an OpenGL shader program.
+///
+/// `Unspecified` is used to signify that this variable is likely just a
+/// placeholder until its information is manually filled in by the programmer
+/// and not by OpenGLProgram.
+class OpenGLShaderVariable {
+public:
+    /// Represents the type of shader variable
+    enum Type {
+        /// Not yet determined, meant to be a temporary placeholder.
+        Unspecified = 0,
+        /// Vertex attributes
+        Attribute,
+        /// Shader Uniforms
+        Uniform
+    };
+
+    /// The name of this attribute in the shader.
+    std::string name;
+    /// The handle to the shader's attribute location.
+    GLint location;
+    /// The number of elements this attribute has (e.g. 3 for a vec3).
+    GLint numElements;
+    /// The type of this attribute. e.g. GL_FLOAT.
+    GLenum valueType;
+    /// The size of the array this variable encapsulates, Typically this is only 1
+    GLint arraySize;
+    
+    /// The type of the variable (Attribute, Uniform, etc.)
+    Type variableType;
+    
+    /// Whether or not `name[i]` is a sub-element of this shader variable
+    bool isArray() const;
+    
+    /// Whether `location` is invalid.
+    bool valid() const;
+    
+    ///
+    OpenGLShaderVariable();
+    /// Creates a named sttribute.
+    OpenGLShaderVariable(const std::string & name, Type variableType = Unspecified);
+    /// Create an attribute linked to a shader.
+    OpenGLShaderVariable(const std::string & name, Type variableType, GLint location, GLint arraySize, GLint numElements, GLenum valueType);
+    
+    /// In-place initialization of this object
+    void init(const std::string & name, Type variableType, GLint location, GLint arraySize, GLint numElements, GLenum valueType);
+    
+    /// For Attributes only: Enables the attribute given at `location`.
+    void glEnableAttribute() const;
+    /// For Attributes only: Disable the attribute given at `location`.
+    void glDisableAttribute() const;
+    
+    /// For Attributes only: Sets the pointer layout of `location`. For more 
+    /// advanced layout settings you should do it manually using opengl.
+    void glSetAttributeLayout() const;
+    
+};
+
+/// Represents a Shader, which is a component of a Program. The principle operation to perform on
+/// a shader is compiling the shader and then checking the compilation status.
+class OpenGLShader : public OpenGLObject {
+public:
+    /// The type of this shader, one of GL_*_SHADER
+    GLenum type;
+    /// Whether this shader is a vertex shader
+    bool isVertexShader() const;
+    /// Whether this shader is a fragment shader
+    bool isFragmentShader() const;
+    /// Whether this shader is a geometry shader
+    bool isGeometryShader() const;
+    
+    /// The supplied path to the source of this shader
+    std::string filePath;
+    /// The supplied source of this shader
+    std::string source;
+    
+    /// Returns the compilation status of this shader. `nil` implies this shader has never been
+    /// allocated.
+    bool compiledSuccessfully() const;
+    
+    ///
+    OpenGLShader();
+    /// Constructs a shader with a given `type` and `source` code.
+    OpenGLShader(GLenum type, const std::string & source);
+    /// Constructs a shader with a given `type` and `source` code.
+    OpenGLShader(GLenum type, const std::string & source, const std::string & filePath);
+    
+    ///
+    void init(GLenum type, const std::string & source, const std::string & filePath);
+    
+    /// Creates a vertex shader with the given source.
+    static std::shared_ptr<OpenGLShader> vertexShaderWithSource(const std::string & source);
+    /// Creates a fragment shader with the given source.
+    static std::shared_ptr<OpenGLShader> fragmentShaderWithSource(const std::string & source);
+    /// Creates a goemetry shader with the given source.
+    static std::shared_ptr<OpenGLShader> geometryShaderWithSource(const std::string & source);
+    
+    /// Tries to create a vertex shader with the given `filePath`.
+    static std::shared_ptr<OpenGLShader> vertexShaderWithFilePath(const std::string & filePath);
+    /// Tries to create a fragment shader with the given `filePath`.
+    static std::shared_ptr<OpenGLShader> fragmentShaderWithFilePath(const std::string & filePath);
+    /// Tries to create a geometry shader with the given `filePath`.
+    static std::shared_ptr<OpenGLShader> geometryShaderWithFilePath(const std::string & filePath);
+    
+protected:
+
+    /// Override this to actuall allocated content
+    virtual std::shared_ptr<GLuint> allocateContent();
+    /// Override this to actually free the content
+    virtual void freeContent();
+    
+public:
+    
+    /// Compiles this shader if there is a supplied `source` for this shader.
+    void compile();
+    
+    /// The current status message of this shader, representing the result of the info log for 
+    /// this shader. Otherwise, if the shader has not been allocated, a message indicating that
+    /// is the case.
+    std::string statusMessage() const;
+};
+
+/// Computed static information that relates OpenGL type information.
+struct OpenGLTypeInfo {
+    /// The OpenGL type
+    GLenum type;
+    /// A human-readable string representation of `type`
+    std::string name;
+    /// The number of bytes of `type`
+    GLsizei size;
+    /// The number of sub-components of this type. Usually is just 1.
+    GLuint numElements;
+    /// The type of the sub-components of this type. Usually == type.
+    GLenum elementType;
+    
+    OpenGLTypeInfo() {}
+    OpenGLTypeInfo(GLenum type, const std::string & name, GLsizei size, GLuint numElements, GLenum elementType) : type(type), name(name), size(size), numElements(numElements), elementType(elementType) {}
+};
+
+/// Represents a Program, which is a collection of Shaders. Programs are built and linked against
+/// the shaders that make them up. Each shader in a program must uniquely represent a particular 
+/// part of the shader pipeline.
+///
+/// Useful online resource: http://antongerdelan.net/opengl/shaders.html
+class OpenGLProgram : public OpenGLObject {
+public:
+    /// The link status of this program.
+    GLboolean linkSuccess() const;
+    
+    /// The validation status of this program.
+    GLboolean validationSuccess() const;
+    
+    /// The shaders that make up this program.
+    std::vector<std::shared_ptr<OpenGLShader>> shaders;
+
+protected:
+    
+    /// Override this to actuall allocated content
+    virtual std::shared_ptr<GLuint> allocateContent();
+
+    /// Override this to actually free the content
+    virtual void freeContent();
+    
+public:
+    
+    /// Builds and links this program, optionally crashing when any particular
+    /// step of the process of compilation and linking fails. This function
+    /// handles allocating the program, compilation each shader, linking them
+    /// together, and then gathering together each of the active uniforms and
+    /// attributes in the program.
+    void build(bool crashOnFailure = false);
+    
+    /// Equivalent to glUseProgram(handle).
+    GLuint setAsActiveProgram();
+    
+    /// Used to restore the last active program, typically from the result of `setAsActiveProgram()`.
+    void restoreActiveProgram(GLuint oldProgram) const;
+    
+    /// The number of attached shaders as reported by OpenGL
+    GLint attachedShaderCount() const;
+    /// The number of active attributes as reported by OpenGL
+    GLint activeAttributeCount() const;
+    /// The number of active uniforms as reported by OpenGL
+    GLint activeUniformCount() const;
+    
+    /// A mapping of active attributes of this program that maps the name of the attribute to its
+    /// reference. This property should only be used after `build()` has been called.
+    std::map<std::string, OpenGLShaderVariable> activeAttributeMap;
+    /// A mapping of active uniforms of this program that maps the name of the uniform to its
+    /// reference. This property should only be used after `build()` has been called.
+    std::map<std::string, OpenGLShaderVariable> activeUniformMap;
+    
+private:
+    
+    /// To be called after `build()`, this function gathers all of the active uniforms and 
+    /// attributes of this program.
+    void gatherAdditionalProgramInfo();
+
+public:
+
+    /// The current status message of this program, representing the result of the info log for
+    /// this program. Otherwise, if the program has not been allocated, a message indicating that
+    /// is the case.
+    std::string statusMessage() const;
+    
+//    /// Connects each attribute name and buffer data pair present in `attribNamesAndBuffers` to the
+//    /// supplied `vao` and the active attributs in this program. After this function is called,
+//    /// one can simply set `vao` as active to bring back up all the connections created here
+//    /// for rendering purposes. 
+//    func connectDataToProgram(vao: OpenGLVertexArrayObject, attribNamesAndBuffers: [String:OpenGLDataBufferObject]) {
+//        let oldProgramHandle = setAsActiveProgram()
+//        let oldVaoHandle = vao.setAsActiveVAO()
+//        
+//        for (attribName, buffer) in attribNamesAndBuffers {
+//            let attrib = activeAttributeMap[attribName]
+//            let oldDboHandle = buffer.setAsActiveDBO()
+//            attrib?.glSetAttributeLayout()
+//            attrib?.glEnableAttribute()
+//            buffer.restoreActiveDBO(oldDboHandle)
+//        }
+//
+//        vao.restoreActiveVAO(oldVaoHandle)
+//        restoreActiveProgram(oldProgramHandle)
+//    }
+};
+
+
+
+#endif /* OpenGLShaders_hpp */
