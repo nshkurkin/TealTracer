c9eb7cbb683a81a4c219ac024099ae6288b32fca
Nikolai Shkurkin <nshkurkin@gmail.com>
2016-04-13 17:01:36 -0700

    Implementation of Oren-Nayar BRDF

--- tealtracer/BRDF.hpp
+++ tealtracer/BRDF.hpp
@@ -42,10 +42,10 @@ public:
         multiplier += finish.ambient;
         multiplier += finish.diffuse * std::max<float>(0, surfaceNormal.dot(toLight));
         
-//        if (finish.roughness > 0.001) {
-//            Eigen::Vector3f halfwayVector = (toLight + toViewer).normalized();
-//            multiplier += finish.specular * std::pow(std::max<float>(0, surfaceNormal.dot(halfwayVector)), 1.0 / finish.roughness);
-//        }
+        if (finish.roughness > 0.001) {
+            Eigen::Vector3f halfwayVector = (toLight + toViewer).normalized();
+            multiplier += finish.specular * std::pow(std::max<float>(0, surfaceNormal.dot(halfwayVector)), 1.0 / finish.roughness);
+        }
         
         output = pigment.color.block<3,1>(0,0) * multiplier;
         
@@ -66,8 +66,32 @@ public:
     virtual RGBf computeColor(const RGBf & source, const Eigen::Vector3f & toLight, const Eigen::Vector3f & toViewer, const Eigen::Vector3f & surfaceNormal) {
         
         RGBf output;
-        float multiplier = 1.0;
+        
+        /// http://ruh.li/GraphicsOrenNayar.html
+        float angleVN = std::acos(surfaceNormal.dot(toViewer));
+        float angleLN = std::acos(surfaceNormal.dot(toLight));
+        
+        float alpha = std::max(angleVN, angleLN);
+        float beta = std::min(angleVN, angleLN);
+        
+        float gamma = (toViewer - surfaceNormal*toViewer.dot(surfaceNormal)).dot(toLight - surfaceNormal*toLight.dot(surfaceNormal));
+    
+        float roughnessSquared = finish.roughness * finish.roughness;
+    
+        // calculate A and B
+        float A = 1.0 - 0.5 * (roughnessSquared / (roughnessSquared + 0.57));
+        float B = 0.45 * (roughnessSquared / (roughnessSquared + 0.09));
+        float C = sin(alpha) * tan(beta);
+        
+        // put it all together
+        float multiplier = std::max<float>(0.0, surfaceNormal.dot(toLight)) * (A + B * std::max<float>(0.0, gamma) * C);
+        
+        ///
         output = pigment.color.block<3,1>(0,0) * multiplier;
+        
+        output.x() *= source.x();
+        output.y() *= source.y();
+        output.z() *= source.z();
         
         return output;
     }
